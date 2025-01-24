e71e4911d366e9bdab3ff4b81590c28099e100b8
VipWW <55458813+VipWW@users.noreply.github.com>
2025-01-25 00:10:10 +0100

    feat: users can delete their messages

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,6 +1,7 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Accounts.User
   alias Slax.Chat
   alias Slax.Chat.{Message, Room}
 
@@ -74,6 +75,7 @@ defmodule SlaxWeb.ChatRoomLive do
         <.message
           :for={{dom_id, message} <- @streams.messages}
           dom_id={dom_id}
+          current_user={@current_user}
           message={message}
           timezone={@timezone}
         />
@@ -123,12 +125,22 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   attr :dom_id, :string, required: true
+  attr :current_user, User, required: true
   attr :message, Message, required: true
   attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
     <div id={@dom_id} class="relative flex px-4 py-3">
+      <button
+        :if={@current_user.id == @message.user_id}
+        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+        data-confirm="Are you sure?"
+        phx-click="delete-message"
+        phx-value-id={@message.id}
+      >
+        <.icon name="hero-trash" class="h-4 w-4" />
+      </button>
       <div class="h-10 w-10 rounded shrink-0 bg-slate-300"></div>
       <div class="ml-2">
         <div class="-mt-1">
@@ -218,5 +230,11 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
     {:noreply, socket}
+  end
+
+  def handle_event("delete-message", %{"id" => id}, socket) do
+    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_user)
+
+    {:noreply, stream_delete(socket, :messages, message)}
   end
 end
